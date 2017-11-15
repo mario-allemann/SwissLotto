de5d94aff853b00787452cba8a93f573e1054c6f
mario-allemann <allemann2@hotmail.com>
2017-11-15 10:24:20 +0100

    test after branch merge

--- SwissLotto/src/estimatingPI/PiMain.java
+++ SwissLotto/src/estimatingPI/PiMain.java
@@ -22,7 +22,6 @@ public class PiMain extends Application {
     private ServiceLocator serviceLocator; // resources, after initialization
 
     public static void main(String[] args) {
-    	
         launch(args);
     }
 
